d9b14e980c42af29202c4bcccaf948b5ecbe5543
Sezgin Mutlu <59778816+sezginmutlu@users.noreply.github.com>
2020-04-01 13:03:52 -0500

    Create from_scratch.groovy

---
+++ from_scratch.groovy
@@ -0,0 +1,46 @@
+node {
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+		
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * *')]),
+		parameters([choice(choices: [
+			'dev1.acirrustech.com', 
+			'qa1.acirrustech.com', 
+			'stage1.acirrustech.com', 
+			'prod1.acirrustech.com'], 
+			description: 'Please choose an environment', 
+			name: 'ENVIR')]), 
+		])
+
+		// Pulls a repo from developer
+	stage("Pull Repo"){
+		git   'https://github.com/farrukh90/cool_website.git'
+	}
+		//Installs web server on different environment
+	stage("Install Prerequisites"){
+		sh """
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
+		"""
+	}
+		//Copies over developers files to different environment
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
+		"""
+	}
+		//Restarts web server
+	stage("Restart web server"){
+		sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
+	}
+
+		//Sends a message to slack
+	stage("Slack"){
+		slackSend color: '#BADA55', message: 'Hello, World!'
+	}
+}
