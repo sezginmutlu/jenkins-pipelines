8d13ced033af396aab1eda4a22e7f92148ea4cca
Sezgin Mutlu <sezginmutlu6@gmail.com>
2020-04-07 18:55:50 -0500

    artemis added deployer and delivery

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -1,58 +1,37 @@
-node { 
-
-properties( 
-
-[parameters( 
-
-[choice(choices:  
-
-[ 
-
-'0.1',  
-
-'0.2',  
-
-'0.3',  
-
-'0.4',  
-
-'0.5'],  
-
-description: 'Which version of the app should I deploy? ',  
-
-name: 'Version')])]) 
-
-stage("Stage1"){ 
-
-timestamps { 
-
-ws { 
-
-checkout([$class: 'GitSCM', branches: [[name: 'dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]]) 
-
-} 
-
-} 
-
-} 
-
-stage("Get Credentials"){ 
-
-timestamps { 
-
-ws{ 
-
-sh ''' 
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 945548589381.dkr.ecr.us-east-1.amazonaws.com/artemis
-
-''' 
-
-} 
-
-} 
-
-} 
-
+node {
+	properties(
+		[parameters(
+			[choice(choices: 
+			[
+				'version/0.1', 
+				'version/0.2', 
+				'version/0.3', 
+				'version/0.4', 
+				'version/0.5'], 
+	description: 'Which version of the app should I deploy? ', 
+	name: 'Version')])])
+	stage("Stage1"){
+		timestamps {
+			ws {
+                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]])		}
+	}
 }
-
+	stage("Get Credentials"){
+		timestamps {
+			ws{
+				sh '''
+					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis
+					'''
+		    }
+	    }
+    }
+    stage("Build Docker Image"){
+		timestamps {
+			ws {
+				sh '''
+					docker build -t artemis:${Version} .
+					'''
+		    }
+	    }
+    }
+}
