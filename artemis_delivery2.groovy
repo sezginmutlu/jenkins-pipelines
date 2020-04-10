d3b1e71ff499a9f051b5bc44f28e347ea2a5682e
Sezgin Mutlu <sezginmutlu6@gmail.com>
2020-04-09 19:00:51 -0500

    packer builder

--- artemis_delivery2.groovy
+++ artemis_delivery2.groovy
@@ -94,6 +94,7 @@ node {
                 timestamps { 
                    ws { 
                     sh ''' 
+                    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 945548589381.dkr.ecr.us-east-1.amazonaws.com/artemis
                     ssh centos@${ENVIR} docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version} 
                      ''' 
               } 
